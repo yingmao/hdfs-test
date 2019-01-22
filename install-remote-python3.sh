96eb6a2413a24c0382be4d9801b9f8d39f0facee
Ying Mao <giggle.my@gmail.com>
2019-01-21 19:57:43 -0500

    update the names

--- install-remote-python3.sh
+++ install-remote-python3.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -rf /hdfs-test/ && cd / && git clone https://github.com/yingmao/hdfs-test.git && python3 /hdfs-test/setup.py
+rm -rf /hdfs-test/ && cd / && git clone https://github.com/yingmao/hdfs-test.git && python3 /hdfs-test/setup3.py
 cp master /hdfs-test/
 cp slaves /hdfs-test/
 cd /hdfs-test/ && ./install-local-python3.sh
