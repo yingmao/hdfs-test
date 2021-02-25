276d5be8c93eb6528b3fb3e7581a97ac61fcedc5
yingmao <giggle.my@gmail.com>
2021-02-24 21:14:49 -0500

    Update setup.py
    ssh localhost

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ sip = sf.read().replace("-","")
 mf.close()
 sf.close()
 
+os.system("cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys")
+
 os.system("apt-get update -y && apt-get install python -y && apt-get install -y default-jdk && apt-get install -y curl && apt-get install -y maven && apt-get install -y python-pip && apt-get install -y python3-pip && curl -fsSL -o- https://bootstrap.pypa.io/3.5/get-pip.py | python3.5 && hash -r && pip install --upgrade pip && pip install pyhdfs")
 
 #clear first
