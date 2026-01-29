dcd06468b17504331ecdb845d47944fada0ad3d9
yingmao <giggle.my@gmail.com>
2026-01-29 12:46:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ do
     else
         ssh root@$line -n "rm -rf /hdfs-test/ && mkdir /hdfs-test/"
         echo "Copy data to $line"
-        scp /hdfs-test/hadoop-3.3.1.tar.gz root@$line:/hdfs-test/ && scp /hdfs-test/setup.py root@$line:/hdfs-test/ && scp /hdfs-test/manager root@$line:/hdfs-test/ && scp /hdfs-test/workers root@$line:/hdfs-test/
+        scp /hdfs-test/hadoop-3.4.2.tar.gz root@$line:/hdfs-test/ && scp /hdfs-test/setup.py root@$line:/hdfs-test/ && scp /hdfs-test/manager root@$line:/hdfs-test/ && scp /hdfs-test/workers root@$line:/hdfs-test/
         echo "Setup $line"
         ssh root@$line -n "cd /hdfs-test/ && python3 setup.py"
         echo "Finished config node $line"
