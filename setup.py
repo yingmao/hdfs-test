0347cb19a552df27d9e0b0e3343c77600c925fce
yingmao <giggle.my@gmail.com>
2026-01-29 10:29:53 -0500

    Update SSH key addition to include all public keys

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ sip = sf.read().replace("-","")
 mf.close()
 sf.close()
 
-os.system("cat ~.ssh/id_rsa.pub >> ~.ssh/authorized_keys")
+os.system("cat ~.ssh/*.pub >> ~.ssh/authorized_keys")
 
 os.system("apt-get update -y && apt-get install python3 -y && sudo apt install python-is-python3 && apt-get install -y default-jdk && apt-get install -y curl && apt-get install -y maven && apt-get install -y python-pip && apt-get install -y python3-pip && curl -fsSL -o- https://bootstrap.pypa.io/pip/3.5/get-pip.py | python3.5 && hash -r && pip install --upgrade pip")
 
