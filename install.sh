c48f558395a15d1d54122a57c322d8872b2acd1d
yingmao <giggle.my@gmail.com>
2026-01-29 15:54:28 -0500

    Refactor install.sh for improved worker setup

--- install.sh
+++ install.sh
@@ -1,16 +1,37 @@
 #!/bin/sh
-python3 /hdfs-test/setup.py
-cat workers | while read line
-do
-    if [ "$line" = "-" ]; then
-        echo "Skip $line"
-    else
-        ssh root@$line -n "rm -rf /hdfs-test/ && mkdir /hdfs-test/"
-        echo "Copy data to $line"
-        scp /hdfs-test/hadoop-3.4.2.tar.gz root@$line:/hdfs-test/ && scp /hdfs-test/setup.py root@$line:/hdfs-test/ && scp /hdfs-test/manager root@$line:/hdfs-test/ && scp /hdfs-test/workers root@$line:/hdfs-test/
-        echo "Setup $line"
-        ssh root@$line -n "cd /hdfs-test/ && python3 setup.py"
-        echo "Finished config node $line"
-        echo "########################################################"
-    fi
+set -e
+
+BASE_DIR="/hdfs-test"
+SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o ConnectTimeout=10"
+
+cd "$BASE_DIR"
+
+# 1) Setup manager locally
+python3 "$BASE_DIR/setup.py" --role manager
+
+# 2) Extract worker IPs from workers file (strip CRLF, trim, skip '-', blank, comments)
+WORKER_IPS=$(
+  sed 's/\r$//' "$BASE_DIR/workers" \
+  | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' \
+  | grep -Ev '^(#|$|-)$'
+)
+
+echo "Workers:"
+printf '%s\n' $WORKER_IPS
+
+copy_payload() {
+  ip="$1"
+  ssh $SSH_OPTS root@"$ip" "rm -rf $BASE_DIR && mkdir -p $BASE_DIR"
+  scp $SSH_OPTS "$BASE_DIR/hadoop-3.4.2.tar.gz" root@"$ip":"$BASE_DIR/"
+  scp $SSH_OPTS "$BASE_DIR/setup.py"           root@"$ip":"$BASE_DIR/"
+  scp $SSH_OPTS "$BASE_DIR/manager"            root@"$ip":"$BASE_DIR/"
+  scp $SSH_OPTS "$BASE_DIR/workers"            root@"$ip":"$BASE_DIR/"
+}
+
+for ip in $WORKER_IPS; do
+  echo "==== Worker $ip ===="
+  copy_payload "$ip"
+  ssh $SSH_OPTS root@"$ip" "cd $BASE_DIR && python3 setup.py --role worker"
+  ssh $SSH_OPTS root@"$ip" "test -x /usr/local/hadoop-3.4.2/bin/hdfs"
+  echo "[OK] $ip"
 done
